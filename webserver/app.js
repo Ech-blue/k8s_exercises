69f6b5546715a09bf8881b4dc881c0556a3e6496
Chima <agwuncha.chima@gmail.com>
2025-06-07 18:55:07 +0200

    Exercise 1.5

--- webserver/app.js
+++ webserver/app.js
@@ -2,8 +2,24 @@ const http = require('http');
 
 const port = process.env.PORT || 3000;
 
+const html = `
+  <html>
+    <head><title>Todo App</title></head>
+    <body>
+      <h1>Welcome to the Todo App</h1>
+      <p>This is a simple web server responding to GET /</p>
+    </body>
+  </html>
+`;
+
 const server = http.createServer((req, res) => {
-  res.end('Todo app server running\n');
+  if (req.method === 'GET' && req.url === '/') {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+  } else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
 
 server.listen(port, () => {
